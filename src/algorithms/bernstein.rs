7cfb178520b862e27533f2ac152fa370f96ad2fa
Lando <lando.schumpich@gmail.com>
2020-01-17 15:45:22 +0100

    Adds all_bernstein function to compute all bernstein polynomials for a fixed u

--- src/algorithms/bernstein.rs
+++ src/algorithms/bernstein.rs
@@ -26,10 +26,31 @@ pub fn compute_bernstein(i: usize, n: usize, u: f64) -> f64 {
     table[n]
 }
 
+/// Computes the n+1 nth-degree Bernstein polynomials
+/// which are nonzero at a fixed u parameter
+pub fn all_bernstein(n: usize, u: f64) -> Vec<f64> {
+    let mut table: Vec<f64> = repeat(1.0).take(n + 1).collect();
+    let u1 = 1.0 - u;
+
+    for j in 1..n {
+        let mut saved = 0.0;
+
+        for k in 0..j {
+            let temp = table[k];
+            table[k] = saved + u1 * temp;
+            saved = u * temp;
+        }
+
+        table[j] = saved;
+    }
+
+    table
+}
+
 #[cfg(test)]
 mod tests {
 
-    use super::compute_bernstein;
+    use super::{compute_bernstein, all_bernstein};
 
     #[test]
     fn compute_bernstein_should_work() {
@@ -40,5 +61,12 @@ mod tests {
         assert_eq!(compute_bernstein(0, 2, u), (1.0 - u).powi(2));
         assert_eq!(compute_bernstein(1, 2, u), 2.0 * u * (1.0 - u));
         assert_eq!(compute_bernstein(2, 2, u), u.powi(2));
+    }
+
+    #[test]
+    fn all_bernstein_should_work() {
+        let u = 0.4;
+
+        assert_eq!(all_bernstein(2, u), vec![1.0 - u, u, 1.0]);
     }
 }
