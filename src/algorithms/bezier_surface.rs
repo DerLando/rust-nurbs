882c83a4758a29970c57d056ef0fb4e013fa55d0
Lando <lando.schumpich@gmail.com>
2020-01-20 00:45:10 +0100

    Added de-casteljau algorithm for surfaces

--- src/algorithms/bezier_surface.rs
+++ src/algorithms/bezier_surface.rs
@@ -1,15 +1,33 @@
 use crate::{
-    algorithms::{horner},
+    algorithms::{horner, de_casteljeau},
     geometry::Vector,
     utils::Matrix,
 };
 
+/// Computes a point on a power-basis tensor-product surface
 pub fn horner_surface(points: Box<dyn Matrix<Vector>>, n: usize, m: usize, u: f64, v: f64) -> Vector {
     let mut b: Vec<Vector> = Vec::new();
     for i in 0..n + 1 {
         b.push(horner(&points.row(i), m, v));
     }
     horner(&b, n, u)
+}
+
+/// Computes a point on a bezier Surface 
+pub fn de_casteljeau_surface(points: Box<dyn Matrix<Vector>>, n: usize, m: usize, u: f64, v: f64) -> Vector {
+    let mut q: Vec<Vector> = Vec::new();
+    if n <= m {
+        for j in 0..m + 1 {
+            q.push(de_casteljeau(&points.row(j), n, u));
+        }
+        de_casteljeau(&q, m, v)
+    }
+    else {
+        for i in 0..n + 1 {
+            q.push(de_casteljeau(&points.column(i), m, v));
+        }
+        de_casteljeau(&q, n, u)
+    }
 }
 
 #[cfg(test)]
